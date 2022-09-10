2b0a402255bbe5e190c2ca75028a89b952f3a56a
Ubuntu <ubuntu@ip-172-31-40-132.ap-south-1.compute.internal>
2022-09-10 22:03:45 +0000

    add

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -13,7 +13,8 @@ var db *gorm.DB
 
 func GetDbUrl() string {
 	db_url_dev := "root:123456789@/blog_db?charset=utf8&parseTime=True&loc=Local"
-	db_url_prod := "sql6516409:ECm75aXYp8@tcp(sql6.freesqldatabase.com)/sql6516409?charset=utf8mb4&parseTime=True&loc=Local"
+	// db_url_prod := "sql6516409:ECm75aXYp8@tcp(sql6.freesqldatabase.com)/sql6516409?charset=utf8mb4&parseTime=True&loc=Local"
+	db_url_prod :=  "nf_remote_two:iiu**syd^4ewdsccnxjsy^@tcp(3.7.144.210)/saroj_test?charset=utf8mb4&parseTime=True&loc=Local"
 	mode := utils.GetDotEnvVariable("MODE")
 
 	if mode == "production" {
